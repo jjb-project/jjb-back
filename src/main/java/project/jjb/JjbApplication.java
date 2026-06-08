ab7a882922f90a294a9f2c874f46c76266fbdb8d
sangjae <sjsj00718@gmail.com>
2026-06-08 11:56:29 +0900

    fix: 오류 수정

--- src/main/java/project/jjb/JjbApplication.java
+++ src/main/java/project/jjb/JjbApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JjbApplication {
-/
 	public static void main(String[] args) {
 		SpringApplication.run(JjbApplication.class, args);
 	}
